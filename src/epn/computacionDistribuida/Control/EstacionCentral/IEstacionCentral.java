139a8d39506e23d51a016372c1ade8e800baae64
itoromoreno <44718948+itoromoreno@users.noreply.github.com>
2019-06-25 11:53:05 -0500

    Estacion Central Interface updated

--- src/epn/computacionDistribuida/Control/EstacionCentral/IEstacionCentral.java
+++ src/epn/computacionDistribuida/Control/EstacionCentral/IEstacionCentral.java
@@ -2,8 +2,6 @@ package epn.computacionDistribuida.Control.EstacionCentral;
 
 public interface IEstacionCentral {
 
-	public void iniciar();
-	public void obtenerEstados();
-	public void notificar();
+	public void recibirEstado();
 	
 }
